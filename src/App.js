b76131020b27950970476456f93c3da92d506d14
Agathe-LAB <agathe@agat.dev>
2024-12-26 10:25:56 -0500

    navbar + landing static

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import './Components/Navbar/Navbar.css'
+// Import necessary modules from React library
+import React from 'react';
+// Import components for routing from react-router-dom library
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// Import custom Navbar component
+import Navbar from './Components/Navbar/Navbar';
+import LandingPage from './Components/LandingPage/LandingPage';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {/* Set up BrowserRouter for routing */}
+        <BrowserRouter>
+          {/* Display the Navbar component */}
+          <Navbar/>
+          {/* Set up the Routes for different pages */}
+          <Routes>
+            {/* Define individual Route components for different pages */}
+            <Route path="/" element={<LandingPage/>}/>
+          </Routes>
+        </BrowserRouter>
+        <LandingPage />
     </div>
   );
 }
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,47 @@
+function Navbar() {
+    const handleClick = () => {
+     // Handle click event
+    };
+    
+    return (
+     <nav>
+      <div className="nav__logo">
+       <img src="/logo.png" alt="logo" className="logo" />
+       <a href="../Landing_Page/LandingPage.html">
+        StayHealthy
+       </a>
+       <span>.</span>
+      </div>
+      <div className="nav__icon" onClick={handleClick}>
+       
+      </div>
+      <ul className="nav__links active">
+       <li className="link">
+        <a href="../Landing_Page/LandingPage.html">Home</a>
+       </li>
+       <li className="link">
+        <a href="../Landing_Page/LandingPage.html">Appointments</a>
+       </li>
+       <li className="link">
+        <a href="../Landing_Page/LandingPage.html">Health Blog</a>
+       </li>
+       <li className="link">
+        <a href="../Landing_Page/LandingPage.html">Reviews</a>
+       </li>
+       <li className="link">
+        <a href="../Sign_Up/Sign_Up.html">
+         <button className="btn1">Sign Up</button>
+        </a>
+       </li>
+       <li className="link">
+        <a href="../Login/Login.html">
+         <button className="btn2">Login</button>
+        </a>
+       </li>
+      </ul>
+     </nav>
+    );
+    }
+    
+    export default Navbar;
+    
